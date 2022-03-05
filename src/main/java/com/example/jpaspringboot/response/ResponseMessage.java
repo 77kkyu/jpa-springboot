cbf7a9f8284f22f7d502bf6366110f5af9df4eb5
77kkyu <77kkyu@gmail.com>
2022-03-05 23:40:03 +0900

    feat ResponseMessage

---
+++ src/main/java/com/example/jpaspringboot/response/ResponseMessage.java
@@ -0,0 +1,24 @@
+package com.example.jpaspringboot.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResponseMessage {
+
+    private int status;
+
+    private String message;
+
+    private Object data;
+
+    public ResponseMessage(ResultCodeSet resultCodeSet, Object data) {
+        this.status = resultCodeSet.code;
+        this.message = resultCodeSet.message;
+        this.data = data;
+    }
+
+
+
+}

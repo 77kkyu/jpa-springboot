631dffc3a438246a7704cf964df57349ef7aee02
77kkyu <77kkyu@gmail.com>
2022-03-07 22:06:21 +0900

    feat TeamService

--- src/main/java/com/example/jpaspringboot/service/TeamService.java
+++ src/main/java/com/example/jpaspringboot/service/TeamService.java
@@ -2,6 +2,8 @@ package com.example.jpaspringboot.service;
 
 import com.example.jpaspringboot.entity.Team;
 import com.example.jpaspringboot.repository.querydsl.TeamRepositorySupport;
+import com.example.jpaspringboot.response.ResponseMessage;
+import com.example.jpaspringboot.response.ResultCodeSet;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,13 +15,13 @@ public class TeamService {
 
     private final TeamRepositorySupport teamRepositorySupport;
 
-    public Team getTeam(Long id) {
+    public ResponseMessage getTeam(Long id) {
         Team team = teamRepositorySupport.findTeam(id);
         Optional<Team> optionalTeam = Optional.ofNullable(teamRepositorySupport.findTeam(id));
         if (optionalTeam.isPresent()) {
-            return team;
+            return new ResponseMessage(ResultCodeSet.SUCCESS_COMMON, team);
         }else { // 에러 처리 해야함
-            return team;
+            return new ResponseMessage(ResultCodeSet.FAIL, team);
         }
     }
 

2e7ac78db2e40ca8704fd5c7906c7ace027fb884
77kkyu <77kkyu@gmail.com>
2022-03-09 22:34:01 +0900

    feat DTO

---
+++ src/main/java/com/example/jpaspringboot/dto/MemberDto.java
@@ -0,0 +1,17 @@
+package com.example.jpaspringboot.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MemberDto {
+
+    private Long id;
+
+    private String userName;
+
+
+}

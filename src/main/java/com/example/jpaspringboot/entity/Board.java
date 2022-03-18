15d864cd11ff489b0daa6a847ad20caa3c89079f
77kkyu <77kkyu@gmail.com>
2022-03-18 17:05:42 +0900

    feat Board

---
+++ src/main/java/com/example/jpaspringboot/entity/Board.java
@@ -0,0 +1,25 @@
+package com.example.jpaspringboot.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "BOARD_ID")
+    private Long id;
+
+    @Column(name = "USER_ID")
+    private Long userId;
+
+}

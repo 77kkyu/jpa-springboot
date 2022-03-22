04cbd6573ab681efcb8f666c8aa61c97310599f7
77kkyu <77kkyu@gmail.com>
2022-03-23 06:03:48 +0900

    feat BoardTest

--- src/test/java/com/example/jpaspringboot/basicTest/BoardTest.java
+++ src/test/java/com/example/jpaspringboot/basicTest/BoardTest.java
@@ -1,10 +1,17 @@
 package com.example.jpaspringboot.basicTest;
 
+import com.example.jpaspringboot.repository.BoardRepository;
+import com.example.jpaspringboot.repository.querydsl.BoardRepositorySupport;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class BoardTest {
 
+    @Autowired
+    private BoardRepositorySupport boardRepositorySupport;
 
+    @Autowired
+    private BoardRepository boardRepository;
 
 }

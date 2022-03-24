099d25a6f4f16ce30aa894fbcc547902628bfa66
77kkyu <77kkyu@gmail.com>
2022-03-24 15:03:33 +0900

    feat BoardService

---
+++ src/main/java/com/example/jpaspringboot/service/BoardService.java
@@ -0,0 +1,14 @@
+package com.example.jpaspringboot.service;
+
+import com.example.jpaspringboot.repository.querydsl.BoardRepositorySupport;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class BoardService {
+
+    private final BoardRepositorySupport boardRepositorySupport;
+    
+
+}

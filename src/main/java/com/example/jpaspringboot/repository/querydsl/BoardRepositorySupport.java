256962114133adc467a7507de4bb875caa17f68a
77kkyu <77kkyu@gmail.com>
2022-03-25 17:58:59 +0900

    feat BoardRepositorySupport orderBy 추가

--- src/main/java/com/example/jpaspringboot/repository/querydsl/BoardRepositorySupport.java
+++ src/main/java/com/example/jpaspringboot/repository/querydsl/BoardRepositorySupport.java
@@ -1,9 +1,15 @@
 package com.example.jpaspringboot.repository.querydsl;
 
 import com.example.jpaspringboot.entity.Team;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
+
+import static com.example.jpaspringboot.entity.QBoard.board;
 
 @Repository
 public class BoardRepositorySupport extends QuerydslRepositorySupport {
@@ -13,6 +19,23 @@ public class BoardRepositorySupport extends QuerydslRepositorySupport {
     public BoardRepositorySupport(JPAQueryFactory jpaQueryFactory) {
         super(Team.class);
         this.jpaQueryFactory = jpaQueryFactory;
+    }
+
+    public enum OrderByType {
+        ASC{
+            @Override
+            public JPAQuery execute(JPAQuery jpaQuery) {
+                return (JPAQuery) jpaQuery.orderBy(new OrderSpecifier<>(Order.ASC, Expressions.numberPath(Long.class, "")));
+            }
+        },
+        ORDER{
+            @Override
+            public JPAQuery execute(JPAQuery jpaQuery) {
+                return (JPAQuery) jpaQuery.orderBy(new OrderSpecifier<>(Order.DESC, board.id));
+            }
+        };
+
+        public abstract JPAQuery execute(JPAQuery jpaQuery);
     }
 
 }

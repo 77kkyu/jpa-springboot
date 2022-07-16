fb9a3f5a5e660a9cc8f2ba8a5cf2e93442b5006f
77kkyu <77kkyu@gmail.com>
2022-07-16 21:14:20 +0900

    test BoardTest

--- src/test/java/com/example/jpaspringboot/basicTest/BoardTest.java
+++ src/test/java/com/example/jpaspringboot/basicTest/BoardTest.java
@@ -1,11 +1,15 @@
 package com.example.jpaspringboot.basicTest;
 
+import com.example.jpaspringboot.entity.AuthCode;
 import com.example.jpaspringboot.entity.Board;
 import com.example.jpaspringboot.repository.BoardRepository;
 import com.example.jpaspringboot.repository.querydsl.BoardRepositorySupport;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -23,6 +27,26 @@ public class BoardTest {
         Board board = boardRepository.getById(3L);
         assertThat(board).isNotNull();
         return board;
+    }
+
+    @Test
+    public void enumTest() {
+        boardRepository.save(Board.builder()
+                .userId(1L)
+                .title("title")
+                .contents("content")
+                .authCode(AuthCode.A)
+                .build());
+    }
+
+    @Test
+    public void enumFindTest() {
+//        Optional<Board> board = boardRepository.findById(1L);
+//        System.out.println(board.get().toString());
+
+        List<Board> findEnum = boardRepository.findAllByAuthCode(AuthCode.A);
+        findEnum.stream().forEach(e -> System.out.println("데이터 : " + e.toString()));
+
     }
 
 }
